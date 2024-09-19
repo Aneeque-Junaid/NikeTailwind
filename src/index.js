7eeaeb28ab2136378a7a0f4dffc670e26a160acd
Aneeque-Junaid <aneequej2002@gmail.com>
2024-09-19 22:35:50 +0500

    Navbar Almost Done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Hero from "./sections/Hero"
+import Footer from "./sections/Footer"
+import CustomerReviews from "./sections/CustomerReviews"
+import PopularProducts from "./sections/PopularProducts"
+import Services from "./sections/Services"
+import SpecialOffer from "./sections/SpecialOffer"
+import Subscribe from "./sections/Subscribe"
+import SuperQuality from "./sections/SuperQuality"
+
+
+export {Hero, Footer, CustomerReviews, PopularProducts, Services, SpecialOffer, SuperQuality, Subscribe}
