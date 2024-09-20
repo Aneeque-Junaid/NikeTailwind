76b049f4fa1f61942a54679bcc71f7aa76d1d41d
Aneeque-Junaid <aneequej2002@gmail.com>
2024-09-20 06:23:23 +0500

    Fixed Shoe Card

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const ShoeCard = ({imgURL, changeBigShoeImg, BigShoeImg}) => {
+const ShoeCard = ({imgURL, changeBigShoeImg, bigShoeImg}) => {
 
   const handleClick = () => {
-    if(BigShoeImg !== imgURL.bigShoe){
+    if(bigShoeImg !== imgURL.bigShoe){
       changeBigShoeImg(imgURL.bigShoe)
     }
   }
 
 
   return (
-    <div className={`border-2 rounded-xl ${BigShoeImg === imgURL.bigShoe ? `border-coral-red` : `border-transparent` } cursor-pointer max-sm:flex-1 `}
+    <div className={`border-2 rounded-xl ${bigShoeImg === imgURL.bigShoe ? "border-coral-red" : "border-transparent" } cursor-pointer max-sm:flex-1 `}
       onClick={handleClick}
     >
       <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
